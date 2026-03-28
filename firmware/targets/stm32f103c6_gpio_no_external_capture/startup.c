3d5515fde86839d06926153938f5c29ae845d017
ali <ali@ez32.com>
2026-03-28 19:25:30 -0400

    Add STM32F103C6T6 Bluepill-like BMDA path

---
+++ firmware/targets/stm32f103c6_gpio_no_external_capture/startup.c
@@ -0,0 +1,40 @@
+extern int main(void);
+
+extern unsigned long _etext;
+extern unsigned long _sdata;
+extern unsigned long _edata;
+extern unsigned long _sbss;
+extern unsigned long _ebss;
+
+void reset_handler(void);
+void default_handler(void);
+
+void nmi_handler(void) __attribute__((weak, alias("default_handler")));
+void hardfault_handler(void) __attribute__((weak, alias("default_handler")));
+
+__attribute__((section(".isr_vector")))
+void (*const vector_table[])(void) = {
+    (void (*)(void))0x20002800,
+    reset_handler,
+    nmi_handler,
+    hardfault_handler,
+};
+
+void reset_handler(void) {
+    unsigned long *src = &_etext;
+    unsigned long *dst = &_sdata;
+    while (dst < &_edata) {
+        *dst++ = *src++;
+    }
+    for (dst = &_sbss; dst < &_ebss; dst++) {
+        *dst = 0;
+    }
+    (void)main();
+    while (1) {
+    }
+}
+
+void default_handler(void) {
+    while (1) {
+    }
+}
